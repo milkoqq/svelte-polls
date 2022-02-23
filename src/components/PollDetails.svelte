27a20ffa800b7a7c732b44e418e419444853b44f
milkoqq <theofanous.dim@gmail.com>
2022-02-23 12:10:05 +0200

    x

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,33 +1,39 @@
 <script>
+  import Card from "../shared/Card.svelte";
   export let poll;
+
+  const addVote = (answer, id) => {};
+
   $: totalVotes = poll.votesA + poll.votesB;
 </script>
 
-<div class="poll_wrapper">
-  <div class="poll_question">{poll.question}</div>
-  <div class="poll_total-votes">
-    {#if totalVotes > 0}
-      Total Votes: {totalVotes}
-    {:else}
-      Total Votes: No votes yet.
-    {/if}
-  </div>
-  <div class="answer_wrapper">
-    <div class="answer answerA">
-      {poll.answerA}
-      <span class="votesA">({poll.votesA})</span>
+<Card>
+  <div class="poll_wrapper">
+    <div class="poll_question">{poll.question}</div>
+    <div class="poll_total-votes">
+      {#if totalVotes > 0}
+        Total Votes: {totalVotes}
+      {:else}
+        Total Votes: No votes yet.
+      {/if}
     </div>
-    <div class="answer answerB">
-      {poll.answerB}
-      <span class="votesB">({poll.votesB})</span>
+    <div class="answer_wrapper">
+      <div class="answer answerA" on:click={addVote(a)}>
+        {poll.answerA}
+        <span class="votesA">({poll.votesA})</span>
+      </div>
+      <div class="answer answerB" on:click={addVote(b)}>
+        {poll.answerB}
+        <span class="votesB">({poll.votesB})</span>
+      </div>
     </div>
   </div>
-</div>
+</Card>
 
 <style>
   .poll_wrapper {
+    border-radius: 8px;
     padding: 1rem;
-    background-color: beige;
   }
   .poll_question {
     font-weight: bold;
@@ -36,8 +42,12 @@
     margin-top: 1rem;
   }
   .answer {
+    cursor: pointer;
     padding: 1rem;
     margin: 8px;
     background-color: #edf2f0;
+  }
+  .answer:hover {
+    opacity: 0.8;
   }
 </style>
---
+++ src/shared/Card.svelte
@@ -0,0 +1,11 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    background-color: beige;
+
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }
+</style>
